43d0fcc96f62ab61ac8a384d4a7bace755653487
Jasurbek <jasurisoqov685@gmail.com>
2022-04-27 16:07:10 +0500

    create VideoItem.js

---
+++ src/components/VideoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class VideoItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default VideoItem;
