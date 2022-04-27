6fdeb9f996689dd0d8df11741fe251039aeba105
Jasurbek <jasurisoqov685@gmail.com>
2022-04-27 16:07:54 +0500

    create VideoList.js

---
+++ src/components/VideoLIst.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import VideoDetail from "./VideoDetail";
+
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { videoData: [] };
+  }
+
+  renderList = () => {
+    return this.props.data.data?.items.map((val) => {
+      return (
+        <div key={val.id.videoId}>
+          <VideoDetail data={val} />
+        </div>
+      );
+    });
+  };
+
+  render() {
+    return <div>{this.renderList()}</div>;
+  }
+}
+
+export default VideoList;
