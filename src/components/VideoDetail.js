ddba2d4f7090d991fe4cff3c4f26cb9e9def8d9d
Jasurbek <jasurisoqov685@gmail.com>
2022-04-28 09:41:43 +0500

    Channel Image

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,17 +5,38 @@ import "../Style/MainStyle.css";
 class VideoDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { arr: "" };
+    console.log(this.props);
+  }
+
+  getChannelInfo = async (id) => {
+    const data = await fetch(
+      `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${id}&fields=items(id%2Csnippet%2Fthumbnails)&key=AIzaSyDXZdLg3XezWfbwftMKP7r2mJyzOf2AO1Q`
+    );
+    const getJson = await data.json();
+    return getJson.items[0].snippet.thumbnails.default.url;
+    console.log(this.props);
+  };
+  async componentDidMount() {
+    let a = await this.getChannelInfo(this.props.data.snippet.channelId);
+    this.setState({ arr: a });
   }
 
   renderVideo = () => {
     return (
       <div className="container">
         <div className="video_picture">
-          <img src={this.props.data.snippet.thumbnails.high.url} />
+          <img src={this.props.data.snippet.thumbnails.medium.url} />
           <div className="data_vid">
             <h2 className="title">{this.props.data.snippet.title}</h2>
             <p>{this.props.data.snippet.publishedAt}</p>
-            <p>{this.props.data.snippet.channelTitle}</p>
+            <div className="channel_image">
+              <img
+                src={this.state.arr}
+                style={{ width: "30px", borderRadius: "20px" }}
+              />
+              <span>{this.props.data.snippet.channelTitle}</span>
+            </div>
             <p className="des">{this.props.data.snippet.description}</p>
           </div>
         </div>
